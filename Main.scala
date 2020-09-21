32eb45b67bc8c5129ceeb998b3cba55ea0f95696
nurayka <62371468+nurayka@users.noreply.github.com>
2020-09-21 23:27:09 +0600

    Add files via upload

---
+++ Main.scala
@@ -0,0 +1,25 @@
+
+object Main extends App {
+
+  def getDecimalValue(head: ListNode): Int = {
+    if (head==0){
+      print((head));
+    }
+     if(head==1){
+       print(head)
+     }
+    print(scala> Integer.parseInt("head",16))
+  }
+  def test1(): Unit = {
+    val head=101
+    print(getDecimalValue(head)
+  }
+  def test2(): Unit = {
+    val head=0
+    print(getDecimalValue(head)
+  }
+  def test1(): Unit = {
+    val head=1
+    print(getDecimalValue(head)
+  }
+}
